da9a10fd0110a9ffd7ee2968223e96dd54c50545
Rauno Tuul <15248710+raunz@users.noreply.github.com>
2025-03-04 11:32:07 +0200

    group flask metrics by endpoint, not by path

--- powerdnsadmin/__init__.py
+++ powerdnsadmin/__init__.py
@@ -13,7 +13,7 @@ def create_app(config=None):
     from .assets import assets
     app = Flask(__name__)
 
-    metrics = GunicornPrometheusMetrics(app, excluded_paths=["/static/*"])
+    metrics = GunicornPrometheusMetrics(app, group_by='endpoint', excluded_paths=["/static/*"])
 
     # Read log level from environment variable
     log_level_name = os.environ.get('PDNS_ADMIN_LOG_LEVEL', 'WARNING')
